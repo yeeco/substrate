9e048aacc2357d7353f221221842e8a579eb3ca9
guoxbin <guoxbin@gmail.com>
2020-10-19 10:58:11 +0800

    useless peer for invalid justification

--- core/network/src/service.rs
+++ core/network/src/service.rs
@@ -99,8 +99,8 @@ impl<B: BlockT, S: NetworkSpecialization<B>> Link<B> for NetworkLink<B, S> {
 	fn justification_imported(&self, who: PeerId, hash: &B::Hash, number: NumberFor<B>, success: bool) {
 		let _ = self.protocol_sender.send(ProtocolMsg::JustificationImportResult(hash.clone(), number, success));
 		if !success {
-			let reason = Severity::Bad(format!("Invalid justification provided for #{}", hash).to_string());
-			// let _ = self.network_sender.send(NetworkMsg::ReportPeer(who, reason));
+			let reason = Severity::Useless(format!("Invalid justification provided for #{}", hash).to_string());
+			let _ = self.network_sender.send(NetworkMsg::ReportPeer(who, reason));
 		}
 	}
 
